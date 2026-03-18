851ec5f198408f792a793bd2e9a7337edfe5ab77
Vedant Jadhav <121371485+vedant2863@users.noreply.github.com>
2026-03-18 10:40:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
 //complete this code
-class Rectangle {}
+class Rectangle {
+	constructor(width, height) {
+		this._width = width;
+		this._height = height;
+	}
 
-class Square extends Animal {}
+	get width() {
+		return this._width;
+	}
 
-// Do not change the code below this line
+	get height() {
+		return this._height;
+	}
+
+	getArea() {
+		return this._width * this._height;
+	}
+}
+
+class Square extends Rectangle {
+	constructor(side) {
+		super(side, side); // call parent constructor
+	}
+
+	getPerimeter() {
+		return 4 * this._width;
+	}
+}
+
+// Do not change below this
 window.Rectangle = Rectangle;
 window.Square = Square;
